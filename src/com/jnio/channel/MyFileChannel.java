8ec15a6dbddc01aea81e63944d12c3571a4626f6
杨训武 <yangxw@tydic.com>
2020-12-03 08:04:50 +0800

    file channel example

---
+++ src/com/jnio/channel/MyFileChannel.java
@@ -0,0 +1,36 @@
+package com.jnio.channel;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author: yangxunwu
+ * @date: 2020/12/3 7:59
+ */
+public class MyFileChannel {
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile file = new RandomAccessFile("resoueces/data.txt", "rw");
+        FileChannel fileChannel = file.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(64);
+        int readBytes = fileChannel.read(buffer);
+        while (readBytes != -1) {
+            System.out.println("Read " + readBytes);
+
+            buffer.flip();
+
+            while (buffer.hasRemaining()) {
+                System.out.print((char) buffer.get());
+            }
+
+            buffer.clear();
+            readBytes = fileChannel.read(buffer);
+        }
+
+        fileChannel.close();
+    }
+
+}
